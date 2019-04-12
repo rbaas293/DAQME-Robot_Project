23c0fc77475a2bdd97aeebe62ed008fd8f54a3c9
Jake Squidz <jdcook96@gmail.com>
2019-04-12 11:13:11 -0400

    added a few branches to if statement

--- LittleBot/LittleBot.ino
+++ LittleBot/LittleBot.ino
@@ -16,37 +16,40 @@ int servoPin2 = 11;//declares pin for servo2
 Servo Servo2;//creates Servo2 object
 
 void setup() {
-
-Servo1.attach(servoPin1);//attaches servo1 to servopin1
-Servo2.attach(servoPin2);//attaches servo2 to servopin2
-
+  Servo1.attach(servoPin1);//attaches servo1 to servopin1
+  Servo2.attach(servoPin2);//attaches servo2 to servopin2
 }
 
 void loop() {
 
-  // Case 1: Left sensor and right sensors are reading white and middle sensor is reading black. Drive forward!
-  if (readQD(lsensor) < whitelevl && readQD(msensor)> blacklevl && readQD(rsensor) < whitelevl)
-  {
-    goforward();
-  }
-  // Case 2 : Left sensor and middle sensor are reading white and right sensor is reading black. Turn right!
-  else if (readQD(lsensor) < whitelevl && readQD(msensor)< whitelevl && readQD(rsensor) > blacklevl)
+  // Case 1 : Right sensors and middle sensor are reading black and left sensors are reading white. Turn right!
+  if (readQD(flsensor) < whitelevl && readQD(mlsensor) < whitelevl && readQD(msensor)< blacklevl && readQD(mrsensor) > blacklevl && readQD(frsensor) > blacklevl)
   {
     while(true)
     {
       turnright();
-      if ((readQD(lsensor) < whitelevl && readQD(msensor)> blacklevl && readQD(rsensor) < whitelevl) || (readQD(lsensor)> blacklevl && readQD(msensor)> blacklevl && readQD(rsensor) < whitelevl))
-        {break;} // Break if Left sensor and right sensor are reading white and middle sensor is reading black
+      if ((readQD(flsensor) < whitelevl && readQD(mlsensor) < whitelevl && readQD(msensor)< blacklevl && readQD(mrsensor) > whitelevl && readQD(frsensor) > whitelevl) || (readQD(flsensor) < whitelevl && readQD(mlsensor) < blacklevl && readQD(msensor)< blacklevl && readQD(mrsensor) > whitelevl && readQD(frsensor) > whitelevl))
+        {break;} // Break if Left sensor and right sensors are reading white and middle sensors are reading black
     }
   }
-  // Case 3 : Left sensor is reading white, middle sensor and right sensor are reading black. Turn right!
-  else if (readQD(lsensor) < whitelevl && readQD(msensor)> blacklevl && readQD(rsensor) > blacklevl)
+  // Case 2 : Right sensors are reading black and left sensors and middle sensor are reading white. Turn right!
+  else if (readQD(flsensor) < whitelevl && readQD(mlsensor) < whitelevl && readQD(msensor)< whitelevl && readQD(mrsensor) > blacklevl && readQD(frsensor) > blacklevl)
   {
     while(true)
     {
       turnright();
-      if ((readQD(lsensor) < whitelevl && readQD(msensor)> blacklevl && readQD(rsensor) < whitelevl) || (readQD(lsensor)> blacklevl && readQD(msensor)> blacklevl && readQD(rsensor) < whitelevl))
-        {break;} // Break if Left sensor and right sensor are reading white and middle sensor is reading black
+      if ((readQD(flsensor) < whitelevl && readQD(mlsensor) < whitelevl && readQD(msensor)< blacklevl && readQD(mrsensor) > whitelevl && readQD(frsensor) > whitelevl) || (readQD(flsensor) < whitelevl && readQD(mlsensor) < blacklevl && readQD(msensor)< blacklevl && readQD(mrsensor) > whitelevl && readQD(frsensor) > whitelevl))
+        {break;} // Break if Left sensor and right sensors are reading white and middle sensors are reading black
+    }
+  }
+  // Case 3 : Far right sensor is reading black and left sensors, middle sensor, and middle right sensor are reading white. Turn right!
+  else if (readQD(flsensor) < whitelevl && readQD(mlsensor) < whitelevl && readQD(msensor)< whitelevl && readQD(mrsensor) > whitelevl && readQD(frsensor) > blacklevl)
+  {
+    while(true)
+    {
+      turnright();
+      if ((readQD(flsensor) < whitelevl && readQD(mlsensor) < whitelevl && readQD(msensor)< blacklevl && readQD(mrsensor) > whitelevl && readQD(frsensor) > whitelevl) || (readQD(flsensor) < whitelevl && readQD(mlsensor) < blacklevl && readQD(msensor)< blacklevl && readQD(mrsensor) > whitelevl && readQD(frsensor) > whitelevl))
+        {break;} // Break if Left sensor and right sensors are reading white and middle sensors are reading black
     }
   }
   // Case 4 : Left sensor is reading black, middle sensor and right sensor are reading white. Turn left!
@@ -72,17 +75,19 @@ void loop() {
   else
   {
     goforward(); // If there is no line, the rover will go forward
-    }
+  }
 }
 void goforward(){
-Servo1.write(180);//tells servo1 to go full speed clockwise
-Servo2.write(180);//tells servo2 to go full speed clockwise
+  Servo1.write(180);//tells servo1 to go full speed clockwise
+  Servo2.write(180);//tells servo2 to go full speed clockwise
 }
-
 
 void turnright(){
+  Servo1.write(180);//tells servo1 to go full speed clockwise
+  Servo2.write(0);//tells servo2 to go full speed counterclockwise
 }
 
-
 void turnleft(){
+  Servo1.write(0);//tells servo1 to go full speed counterclockwise
+  Servo2.write(180);//tells servo2 to go full speed clockwise
 }
